fd9affcad88654b7ded6b5d93d9dc61223752e97
morfredus <morfredus@gmail.com>
2025-11-25 17:30:43 +0100

    Change pin mapping

--- include/config.h
+++ include/config.h
@@ -49,9 +49,9 @@
 #define TFT_MOSI 11                  // SPI MOSI pin
 #define TFT_SCLK 12                  // SPI Clock pin
 #define TFT_CS 10                    // Chip Select pin
-#define TFT_DC 9                     // Data/Command pin
-#define TFT_RST 46                   // Reset pin
-#define TFT_BL 48                    // Backlight pin (-1 to disable)
+#define TFT_DC 6                     // Data/Command pin
+#define TFT_RST 7                   // Reset pin
+#define TFT_BL 15                    // Backlight pin (-1 to disable)
 #define TFT_ROTATION 0               // 0, 1, 2, or 3 (90° increments)
 
 // ========== LED CONFIGURATION ==========
